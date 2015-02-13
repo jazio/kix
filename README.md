cba578f2fe473f2fe4860d402e4cad4d455f7bf3
Ovi Farcas <ovidiu@jazio.net>
2015-02-13 22:47:40 +0100

    Template data fixes

--- README.md
+++ README.md
@@ -1,12 +1,1 @@
-#An intro to PHP Object Oriented Programming
-
-Authentication system.
-
-Database connection using PDO.
-Integrate with Twig
-
-Install the Twig C extension: http://twig.sensiolabs.org/doc/installation.html
-
-Pass codesniffer
-
-Integrate with phpUnit
+##A progressive incomplete intro to PHP Object Oriented Programming.
--- lib/FormValidator.php
+++ lib/FormValidator.php
@@ -6,10 +6,7 @@ namespace lib;
  * Date: 16.10.14
  * Time: 19:04
  */
-    // watch this https://www.youtube.com/watch?v=netHLn9TScY&list=PLfdtiltiRHWGQrNSFxCE9mxspdpX_JleP
-// Refactor based on rules -- see video
-//http://stackoverflow.com/questions/737385/easiest-form-validation-library-for-php
-// http://cipriancociorba.com/php-form-validation-part-2-building-the-validator-class/
+
 class FormValidator {
     public $field;
     public $type;
@@ -22,29 +19,5 @@ class FormValidator {
         }
         return true;
     }
-        // Field specific tests
-        /*
-        switch ($type) {
-            case 'text':
-                if (strlen($field) < 3) {
-                    //@todo replace $field with field name
-                    $this->err[] = "Field {$field} must be minimum 3 characters";
-                    return false;
-                }
-                break;
-            case 'password':
 
-                break;
-            default:
-
-                break;
-        }
-    return true;
-    }
-
-
-    public function setError($errors) {
-        $this->errors[] = $errors;
-    }
-*/
 }
